b9b9ca44552bc9e9198d798e75e591ade59def4d
Muhammad Jawad <77524488+mj-awad17@users.noreply.github.com>
2024-05-03 22:03:34 +0500

    First-Module

---
+++ operate.py
@@ -0,0 +1,15 @@
+# addition function
+def addition(x, y):
+    return x + y
+
+# subtraction function
+def subtraction(x, y):
+    return x - y
+
+# multiplication function
+def multiplication(x, y):
+    return x * y
+
+# division function
+def division(x, y):
+    return x // y
